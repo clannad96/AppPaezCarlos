0e1f7f97ed6788bb0b67355530b8e7c6518af396
Administrador <Administrador@cm.com>
2022-12-14 16:44:21 -0300

    Segunda App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-//var axios = require('axios');
+var axios = require('axios');
 //app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.set('port', process.env.PORT || 3001);
@@ -14,9 +14,31 @@ app.listen(app.get('port'), () => {
 
 
   
-  app.post("/vermensaje", (req, res) => {
-    var candidato= "Lolocar";
-    if(req.body.candidato)
-        candidato=req.body.candidato;
-    res.json({ message: "Hola, "+candidato });
-  });
+  app.post("/vermensaje", async (req, res) => {
+    try{
+    var nombre= "Lolocar";
+    var edad = "0";
+    if(req.body.nombre)
+        nombre=req.body.nombre.toString();
+
+        var config = {
+            method: 'get',
+            url: 'https://api.agify.io/?name='+nombre,
+            headers: { }
+          };
+          
+          console.log(config);
+          var response = await axios(config);
+        //  console.log(response.data)
+          console.log(JSON.stringify(response.data))
+        if(response.data.age)
+          edad=response.data.age;
+
+    
+    res.json({ message: "Hola, "+nombre+". Tu nombre tiene "+edad+" años." });
+    }
+    catch(error){
+        console.log(error);
+        res.json(error);
+    }
+});
